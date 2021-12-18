c36f83a43fcd3ac08405a5b11ce6a87a7bcfa7c0
MaximPiskun <maxpropiskun@yandex.ru>
2021-12-18 16:39:03 +0700

    io

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def weather():
 
 
     URL = 'https://weather.rambler.ru/v-novosibirske/tomorrow/'
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 YaBrowser/20.3.1.197 Yowser/2.5 Safari/537.36'}
+    HEADERS = {''}
     item = 0
     item1 = 0
 
@@ -39,7 +39,7 @@ def weather():
 
 
 
-bot = telebot.TeleBot('1954027996:AAE7a6Js8XQVQC7wBzDAwwFuylVGm39gksE')
+bot = telebot.TeleBot('')
 controller = {}
 
 button_1 = types.KeyboardButton('Погода сегодня')
